7220da6dbd91a15f2c276d137d67c537365c3e13
thealinasanchez <74123101+thealinasanchez@users.noreply.github.com>
2023-07-12 16:17:52 +0000

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,108 @@
 Vue.createApp({
     data() {
         return {
+            organizations: [],
+            search: "",
+            filteredOrganizations: [],
+            sortNames: "",
+            newOrganization: {
+                name: "",
+                location: "",
+                missionStatement: ""
+            }
         }
     },
-    methods: {
-        // LONGITUDE AND LATITUDE FUNCTIONS
-        getLocation: function () {
-            var x = document.getElementById("user-location");
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+    methods : {
+        getOrganizations: function() {
+            fetch("http://localhost:8080/organizations")
+            .then((response) => response.json())
+            .then((data) => {
+                this.organizations = data;
+            });
+        },
+        resetSearch: function() {
+            this.search = "";
+        },
+        sortNames: function() {
+            if (this.sortNames == 'asc') {
+                function compare(a,b) {
+                    if (a.amount > b.amount) return -1;
+                    if (a.amount < b.amount) return 1;
+                    return 0;
+                }
+                this.sortNames = 'desc';
             } else {
-                x.innerHTML = "Geolocation is not supported by this browser.";
+                function compare(a,b) {
+                    if (a.amount < b.amount) return -1;
+                    if (a.amount > b.amount) return 1;
+                    return 0;
+                }
+                this.sortNames = 'asc';
             }
+            this.organizations.sort(compare);
         },
-        showPosition: function (position) {
-            var x = document.getElementById("user-location");
-            x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+        updateOrganization: function() {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+            
+            var encodedData = "name=" + encodeURIComponent(this.newOrganization.name) +
+                                "&location=" + encodeURIComponent(this.newOrganization.location) +
+                                "&missionStatement" + encodeURIComponent(this.newOrganization.missionStatement);
+            var requestOptions = {
+                method: 'PUT',
+                body: encodedData,
+                headers: myHeaders
+            };
+            var orgId = this.expenses[this.modal.index]._id;
+            console.log(orgId);
+            fetch(`http://localhost:8080/organizations/${orgId}`, requestOptions)
+            .then((response) => {
+                if (response.status == 204) {
+                    this.organizations[this.modal.index].name = this.newOrganization.name;
+                    this.organizations[this.modal.index].location = this.newOrganization.location;
+                    this.organizations[this.modal.index].missionStatement = this.newOrganization.missionStatement;
+                }
+            })
         },
-        showError: function (error) {
-            var x = document.getElementById("user-location");
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    x.innerHTML = "User denied the request for Geolocation.";
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    x.innerHTML = "Location information is unavailable.";
-                    break;
-                case error.TIMEOUT:
-                    x.innerHTML = "The request to get user location timed out.";
-                    break;
-                case error.UNKOWN_ERROR:
-                    x.innerHTML = "An unknown error occured.";
-                    break;
-            }
-        },
-        // REVERSE GEOCODING FUNCTIONS
+        addOrganization: function() {
+            myHeaders = new Headers();
+            // first param is the header, second param is content of header
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
+            var encodedData = "name=" + encodeURIComponent(this.modal.name) +
+                                "&location=" + encodeURIComponent(this.modal.location) +
+                                "&missionStatement" + encodeURIComponent(this.modal.missionStatement);
+            var requestOptions = {
+                method: 'POST',
+                body: encodedData,
+                headers: myHeaders
+            };
+            fetch("http://localhost:8080/organizations", requestOptions)
+            .then((response) => {
+                if(response.status === 201) {
+                    response.json().then(data => {
+                        this.organizations.push(data);
+                        this.newOrganization = {};
+                    })
+                    console.log("Success");
+                    this.newOrganization = {};
+                } else {
+                    alert("Not able to add organization");
+                }
+            })
+        }
     },
-    created: function () {
+    created : function() {
+        this.getOrganizations();
+    },
+
+    watch: {
+        search(newSearch, oldSearch) {
+            this.filteredOrganizations = this.organizations.filter((org) => {
+                return org.description
+                    .toLowerCase()
+                    .includes(newSearch.toLowerCase());
+            });
+        }
     }
 }).mount("#app");
