6cea0940a1f4e19dba4aa1e65441cd4a6f4316ae
thealinasanchez <74123101+thealinasanchez@users.noreply.github.com>
2023-07-12 16:43:38 +0000

    index.js

---
+++ server/index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const cors = require('cors');
+const model = require('./model');
+
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+// GET
+app.get("/organizations", function(req, res) {
+    model.JournalEntry.find().then(entries => {
+        res.json(entries);
+    });
+});
+
+app.get("/organizations/:orgId", function(req,res) {
+    model.JournalEntry.findOne({"_id":req.params.orgId}).then(expense => {
+        if (expense) {
+            res.json(expense);
+        }
+        else {
+            console.log("Organization not found.");
+            res.status(404).send("Organization not found.");
+        }
+    }).catch(() => {
+        console.log("Bad request (GET by ID).");
+        res.status(400).send("Organization not found.");
+    })
+});
+
+// POST
+app.post("/organizations", function(req,res) {
+    const newEntry = new model.JournalEntry({
+        name: req.body.name,
+        location: req.body.location,
+        mission: req.body.missionStatement
+    });
+
+    newEntry.save().then(() => {
+        console.log("New organization/journal entry added.");
+        res.status(201).send(newEntry);
+    }).catch((errors) => {
+        let error_list = [];
+        for (var key in errors.errors) {
+            error_list.push(errors.errors[key].message)
+        }
+        res.status(422).send(error_list);
+    })
+})
+
+// PUT
+app.put("/organizations:orgId", function(req,res) {
+    const updatedOrg = {
+        name: req.body.name,
+        location: req.body.location,
+        mission: req.body.missionStatement
+    }
+
+    model.JournalEntry.findByIdAndUpdate({"_id": req.params.orgId}, updatedOrg, {"new": true}).then(org => {
+        if (org) {
+            res.status(204).send("Organization updated.");
+        }
+        else {
+            res.status(404).send("Organization not found.");
+        }
+    }).catch(() => {
+        res.status(422).send("Unable to update.");
+    });
+});
+
+// DELETE
+app.delete("/organizations/orgId", function(req,res) {
+    model.JournalEntry.findOneAndDelete({"_id":req.params.orgId}).then(org => {
+        if(org) {
+            res.status(204).send("Organization deleted.");
+        }
+        else {
+            res.status(404).send("Organization not found.");
+        }
+    }).catch(() => {
+        res.status(422).send("Unable to delete.");
+    });
+});
+
+app.listen(port, function() {
+    console.log(`Running server on port ${port}...`);
+});
