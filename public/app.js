8d9e30eb28f5a59ea7e452f350fdfc3f58f78fdc
Thomas Keate <d00439809@utahtech.edu>
2023-07-14 09:47:09 -0600

    Move app.js

--- public/app.js
+++ public/app.js
@@ -1,14 +1,6 @@
 Vue.createApp({
     data() {
         return {
-            currentClientInformation: {
-                location: {
-                    city: "",
-                    state: "",
-                    lat: "",
-                    long: ""
-                }
-            },
             getStartedForm: {
                 open: false
             },
@@ -33,8 +25,18 @@ Vue.createApp({
                     }
                 ],
             },
+            // organizations.html database variables
             organizations: [],
             search: "",
+            filteredOrganizations: [],
+            sortNames: "",
+            newOrganization: {
+                orgname: "",
+                categories: [],
+                city: "",
+                state: "",
+                missionStatement: ""
+            }
         }
     },
     methods: {
@@ -65,46 +67,117 @@ Vue.createApp({
             this.moveSlideShow();
         },
         // organizations.html database functions
-        getOrganizations: function () {
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", "Basic dGhlYWxpbmFzYW5jaGV6QGdtYWlsLmNvbTphbGluYXNhbmNoZXo4MDE=");
-            myHeaders.append("Content-Type", "application/json");
+        getOrganizations: async function () {
 
-            fetch("http://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica", {
-                method: "GET",
-                headers: myHeaders,
-                cors: "cors"
-            })
-                .then((response) => {
-                    console.log(response);
-                    return response.json()
-                })
-                .then((data) => {
-                    console.log(data);
-                    this.organizations = data;
-                });
-            console.log(this.organizations);
+
+
+            //   proxyUrl: 'http://localhost:8080/api', // URL of your proxy server
+            //   apiUrl: 'http://api.example.com', // URL of the target API
+            //   responseData: null // Placeholder for the response data
+
+            try {
+                const response = await fetch(`http://localhost:8080/api?url=https://projects.propublica.org/nonprofits/api/v2/search.json?q=`);
+                const data = await response.json();
+                console.log(data);
+                // this.responseData = data;
+            } catch (error) {
+                console.error(error);
+            }
+
+
         },
         resetSearch: function () {
             this.search = "";
         },
-        submitSearch: function () {
-            fetch(`https://projects.propublica.org/nonprofits/api/v2/search.json?q=${this.search.replace(" ", "+")}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log(data);
-                    this.organizations = data;
-                });
+        sortNames: function () {
+            if (this.sortNames == 'asc') {
+                function compare(a, b) {
+                    if (a.amount > b.amount) return -1;
+                    if (a.amount < b.amount) return 1;
+                    return 0;
+                }
+                this.sortNames = 'desc';
+            } else {
+                function compare(a, b) {
+                    if (a.amount < b.amount) return -1;
+                    if (a.amount > b.amount) return 1;
+                    return 0;
+                }
+                this.sortNames = 'asc';
+            }
+            this.organizations.sort(compare);
         },
+        updateOrganization: function () {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+            var encodedData = "orgname=" + encodeURIComponent(this.newOrganization.orgname) +
+                "&categories=" + encodeURIComponent(this.newOrganization.categories) +
+                "&city=" + encodeURIComponent(this.newOrganization.city) +
+                "&state=" + encodeURIComponent(this.newOrganization.state) +
+                "&missionStatement" + encodeURIComponent(this.newOrganization.missionStatement);
+            var requestOptions = {
+                method: 'PUT',
+                body: encodedData,
+                headers: myHeaders
+            };
+            var orgId = this.expenses[this.newOrganization.index]._id;
+            console.log(orgId);
+            fetch(`http://localhost:8080/organizations/${orgId}`, requestOptions)
+                .then((response) => {
+                    if (response.status == 204) {
+                        this.organizations[this.newOrganization.index].orgname = this.newOrganization.orgname;
+                        this.organizations[this.newOrganization.index].categories = this.newOrganization.categories;
+                        this.organizations[this.newOrganization.index].city = this.newOrganization.city;
+                        this.organizations[this.newOrganization.index].state = this.newOrganization.state;
+                        this.organizations[this.newOrganization.index].missionStatement = this.newOrganization.missionStatement;
+                    }
+                })
+        },
+        addOrganization: function () {
+            myHeaders = new Headers();
+            // first param is the header, second param is content of header
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+            var encodedData = "orgname=" + encodeURIComponent(this.newOrganization.orgname) +
+                "&categories=" + encodeURIComponent(this.newOrganization.categories) +
+                "&city=" + encodeURIComponent(this.newOrganization.city) +
+                "&state=" + encodeURIComponent(this.newOrganization.state) +
+                "&missionStatement" + encodeURIComponent(this.newOrganization.missionStatement);
+            var requestOptions = {
+                method: 'POST',
+                body: encodedData,
+                headers: myHeaders
+            };
+            fetch("http://localhost:8080/organizations", requestOptions)
+                .then((response) => {
+                    if (response.status === 201) {
+                        response.json().then(data => {
+                            this.organizations.push(data);
+                            this.newOrganization = {};
+                        })
+                        console.log("Success");
+                        this.newOrganization = {};
+                    } else {
+                        alert("Not able to add organization");
+                    }
+                })
+        }
     },
     created: function () {
         this.getOrganizations();
     },
     watch: {
-
+        search(newSearch, oldSearch) {
+            this.filteredOrganizations = this.organizations.filter((org) => {
+                return org.description
+                    .toLowerCase()
+                    .includes(newSearch.toLowerCase());
+            });
+        }
     },
     mounted: function () {
-        // var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        // var Stadia_AlidadeSmoothDark = new L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
         //     maxZoom: 20,
         //     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OSM</a>'
         // });
