ce8dff5d71a895fb82f2da00105bc56dd050b360
thealinasanchez <thealinasanchez@gmail.com>
2023-07-28 11:31:26 -0600

    /

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-// var URL = "http://localhost:6300/";
-var URL = "https://aidhub-production.up.railway.app/";
+var URL = "http://localhost:6300/";
+// var URL = "https://aidhub-production.up.railway.app/";
 Vue.createApp({
     data() {
         return {
